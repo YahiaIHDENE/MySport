c4e916f56a3cc021541a2534b5c37f15fa5ed8b8
Yahia1995 <yahia.ihdene@outlook.com>
2019-12-05 13:57:35 +0100

    dossier code 1er version

---
+++ MySport/app/src/main/java/com/example/mysport/Inscription.java
@@ -0,0 +1,51 @@
+package com.example.mysport;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class Inscription extends AppCompatActivity {
+// ...
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button btn = (Button) findViewById(R.id.btnInscription);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //Connection à la base de données pour insérer les données
+
+                startActivity(new Intent(Inscription.this, MainActivity.class));
+            }
+        });
+
+
+    }
+
+}
