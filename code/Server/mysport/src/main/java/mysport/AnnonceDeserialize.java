033dfe932ead6d814e5c03a0efb45723e2c4e6d5
wenbi <linw96@gmail.com>
2019-12-15 12:41:11 +0100

    nouvel version de Annonce Desrialier

--- code/Server/mysport/src/main/java/mysport/AnnonceDeserialize.java
+++ code/Server/mysport/src/main/java/mysport/AnnonceDeserialize.java
@@ -1,11 +1,9 @@
 package mysport;
 
+
 import com.google.gson.*;
 
 import java.lang.reflect.Type;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 public class AnnonceDeserialize implements JsonDeserializer<Annonce> {
     // pour s'adpter au tout type de item(objet abstrait), on utilise cette classe permet de décerialisation de string de json,
@@ -16,27 +14,25 @@ public class AnnonceDeserialize implements JsonDeserializer<Annonce> {
         JsonObject json = jsonElement.getAsJsonObject();
         FactoryItem f = new FactoryItem();
         String typeItem = json.get("typeItem").getAsString();
-        Item i = f.getInstanceItem(typeItem);
-        // TODO : annonce json .get Terrain  va être modifier en item
 
-        Item terr = (Item) new Gson().fromJson(json.get("terrain"), i.getClass());
-        System.out.println("tterain "+ terr.getAdresse());
-        String datedebut = json.get("dateDisponible").getAsString();
-        // TODO : peut etre modifier, le format de date
-        SimpleDateFormat ft = new SimpleDateFormat("MMM dd, yyyy, hh:mm:ss a", Locale.US);
-        java.util.Date tmpDateDebut =new Date();
-        java.util.Date tmpCreneau=new Date();
-        try {
-            tmpDateDebut = ft.parse(datedebut);
-            tmpCreneau= ft.parse(json.get("creneau").getAsString());
-        }catch (Exception e) {};
+        Gson gson=new Gson();
+
+
+        Item i = f.getInstanceItem(typeItem);
+        Item terr =  new Gson().fromJson(json.get("item"), i.getClass());
+
+        String receivedannonces = gson.toJson(terr);
+        System.out.println(receivedannonces);
         Annonce annonce = new Annonce();
-        annonce.setDateDisponible(tmpDateDebut);
-        annonce.setTerrain(terr);
+        annonce.setIdAnnonce(json.get("idAnnonce").getAsInt());
+        annonce.setDateDisponible(json.get("dateDisponible").getAsString());
+        annonce.setHeureDebut(json.get("heureDebut").getAsString());
+        annonce.setHeureFin(json.get("heureFin").getAsString());
+        annonce.setItem(terr);
         annonce.setTypeItem(typeItem);
-        annonce.setCreneau(tmpCreneau);
-        annonce.setIdUser(json.get("idUser").getAsInt());
+        annonce.setId_userPublier(json.get("id_userPublier").getAsInt());
         annonce.setNombreDePlaceRestant(json.get("nombreDePlaceRestant").getAsInt());
         return annonce;
     }
+
 }
